b8383b179a3813c479146d34390ce4d132437888
carlosdelolmo <carlos.delolmo.borras@gmail.com>
2022-10-03 16:37:53 +0200

    completely functional but not graphical

--- src/main/java/org/example/PiRunner.java
+++ src/main/java/org/example/PiRunner.java
@@ -1,8 +1,4 @@
-package org.example;
-
 import java.io.*;
-import java.nio.Buffer;
-import java.util.Scanner;
 
 public class PiRunner {
     private String wantedSequence;
@@ -15,28 +11,32 @@ public class PiRunner {
         this.wantedSequence = newWantedString;
     }
 
-    public void search() throws IOException {
-        File file = SingletonPiFile.getTFile();
+    public int search() throws IOException {
+        File file = SingletonPiFile.getMFile();
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
-        // Scanner reader = new Scanner(file);
-        boolean found = false;
         int firstPos = -1;
         int posInComp = 0;
         int currentPos = 0;
-        char currentNumber;
-        while(!found && (currentNumber = (char) br.read()) != 0){
+        int next;
+        for(int i = 0; i < 2; i++) br.read();
+        while((next = br.read()) != -1){
+            char currentNumber = (char) next;
             if(currentNumber == wantedSequence.charAt(posInComp)){
+                // System.out.println("En pos "+ currentPos + " se compara y se ve que es igual " + currentNumber);
                 if(posInComp == 0) firstPos = currentPos;
-                posInComp++;
+                if(++posInComp == wantedSequence.length()){
+                    break;
+                }
             } else {
                 if(posInComp > 0){
                     firstPos = -1;
                     posInComp = 0;
                 }
             }
-            currentPos++;
 
+            currentPos++;
         }
+        return firstPos;
     }
 }
